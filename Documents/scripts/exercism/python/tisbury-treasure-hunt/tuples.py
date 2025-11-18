1809413524ca070a9e75329f176672098b226c3b
tcrz2xc <149424504+tcrz2xc@users.noreply.github.com>
2025-11-18 13:32:05 -0600

    Updates: treasure hunt and cater-waiter were addedii

---
+++ Documents/scripts/exercism/python/tisbury-treasure-hunt/tuples.py
@@ -0,0 +1,73 @@
+"""Functions to help Azara and Rui locate pirate treasure."""
+
+
+def get_coordinate(record):
+    """Return coordinate value from a tuple containing the treasure name, and treasure coordinate.
+
+    :param record: tuple - with a (treasure, coordinate) pair.
+    :return: str - the extracted map coordinate.
+    """
+    return record[-1]
+    
+
+
+def convert_coordinate(coordinate):
+    """Split the given coordinate into tuple containing its individual components.
+
+    :param coordinate: str - a string map coordinate
+    :return: tuple - the string coordinate split into its individual components.
+    """
+    return tuple(coordinate)
+    
+
+
+def compare_records(azara_record, rui_record):
+    """Compare two record types and determine if their coordinates match.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, tuple(coordinate_1, coordinate_2), quadrant) trio.
+    :return: bool - do the coordinates match?
+    """
+    cord1= get_coordinate(azara_record)
+    cord2=""
+    for _ in rui_record[-2]:
+        cord2+=_
+    if cord1==cord2:
+        return True
+    
+    return False
+    
+
+
+def create_record(azara_record, rui_record):
+    """Combine the two record types (if possible) and create a combined record group.
+
+    :param azara_record: tuple - a (treasure, coordinate) pair.
+    :param rui_record: tuple - a (location, coordinate, quadrant) trio.
+    :return: tuple or str - the combined record (if compatible), or the string "not a match" (if incompatible).
+    """
+    if compare_records(azara_record, rui_record):
+        return azara_record + rui_record
+    
+    return "not a match"
+    
+
+
+def clean_up(combined_record_group):
+    """Clean up a combined record group into a multi-line string of single records.
+
+    :param combined_record_group: tuple - everything from both participants.
+    :return: str - everything "cleaned", excess coordinates and information are removed.
+
+    The return statement should be a multi-lined string with items separated by newlines.
+
+    (see HINTS.md for an example).
+    """
+    clean_tuple =[]
+    for record in combined_record_group:
+        clean = record[:1]+ record[2:]
+        clean_tuple.append(clean) 
+        #string representation of the tuple
+        result = [repr(_) for _ in clean_tuple]
+    
+    return "\n".join(result)+"\n"
