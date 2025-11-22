db1fba0807d1d26dd5ec161d7751f3cac60fe3b6
tcrz2xc <149424504+tcrz2xc@users.noreply.github.com>
2025-11-22 17:40:07 -0600

    Delete exercism/python directory

--- exercism/python/pangram/pangram.py
+++
@@ -1,7 +0,0 @@
-import re
-def is_pangram(sentence):
-    cleaned_sentence = re.sub(r'[^a-zA-Z]', "", sentence)
-    cleaned_sentence = cleaned_sentence.lower() #make everything lowercase to not worry about casing
-    letter = set(cleaned_sentence)
-    abc= set("abcdefghijklmnopqrstuvwxyz")
-    return abc.issubset(letter)
